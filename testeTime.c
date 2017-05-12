6d874fa7d654e098d9b8b4e3226de24b5547d101
padilha <jpadineto@outlook.com>
2017-05-12 12:50:38 -0300

    trabalho fork arion

---
+++ testeTime.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+//Função para fazer as iterações 
+double calculaPi(int itera){
+
+	double pi = 2.0;
+	double numerador = 2;
+	double denominador = 1;
+
+
+	for (int i = 1; i <= itera ;i++){
+    	//printf ("entrou no for\n ");
+    
+    		if( i % 2==0){
+    		//printf ("entrou no if par \n");    
+    		numerador = numerador + 2;
+       		pi = pi * (numerador/denominador);    
+        	//printf(" pi do par %f\n",pi);
+    		}else{
+        	//printf ("entrou no else \n");
+        	denominador = denominador + 2;
+        	pi = pi * (numerador/denominador);
+           // printf(" pi do IMpar %f\n",pi);
+    		}
+	}
+
+printf("-----------------------------------------\n");
+printf("| VALOR DE PI | = %f \n",pi*2);
+return (pi*2);   
+
+}
+//Fim da função
+
+void main(void){
+	struct timeval tempo1,tempo2;
+	struct timezone tzp;
+	double tempo, tempoMedio, soma =0;
+	int cont = 1;
+    int i, itera = 200000;
+
+
+    for ( int j =0; j<5 ; j++){
+  	gettimeofday(&tempo1,&tzp);  	
+  	calculaPi(itera);
+  	gettimeofday(&tempo2,&tzp);
+    tempo= (double)(tempo2.tv_sec-tempo1.tv_sec)+(((double)(tempo2.tv_usec-tempo1.tv_usec))/1000000);
+    soma=soma+tempo;
+    printf("\n| TEMPO DECORRIDO DE %d EXECUÇÃO PARA APROXIMAÇÃO DO PI | = %f\n",cont,tempo);
+    cont++;
+    }
+    //printf("soma = %f\n", soma);
+   tempoMedio = soma / 5;
+  
+  printf("\n\n----------------------------------------------------\n");
+  printf("| TEMPO MÉDIO DECORRIDO PARA APROXIMAÇÃO DO PI EXECUTADO 5 VEZES| = %f\n",tempoMedio);
+  printf("----------------------------------------------------\n");
+  printf("| NUMERO DE ITERAÇÕES NECESSARIAS | = %d\n",itera);
+}
+
+
+
+
+
